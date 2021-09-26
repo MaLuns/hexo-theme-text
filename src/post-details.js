febf52fd31053c821b284da13c0fd46aba2e97c1
白云苍狗 <admin@imalun.com>
2021-09-26 11:11:57 +0800

    upd: update

--- src/post-details.js
+++ src/post-details.js
@@ -1,17 +1,17 @@
 function postDetails() {
     $(document).ready(function () {
+        if (window.CONFIG.isToc) {
+            $('#post-toc a').on('click', function (e) {
+                e.preventDefault();
+                var offset = $(this.getAttribute('href')).offset().top;
 
-        $('#post-toc a').on('click', function (e) {
-            e.preventDefault();
-            var offset = $(this.getAttribute('href')).offset().top;
-
-            $("#post-toc .current").removeClass("current");
-            $(this).addClass('current')
-            $('html, body').stop().animate({
-                scrollTop: offset - 100
-            }, 500);
-        });
-
+                $("#post-toc .current").removeClass("current");
+                $(this).addClass('current')
+                $('html, body').stop().animate({
+                    scrollTop: offset - 100
+                }, 500);
+            });
+        }
         mediumZoom('article img', {
             margin: 24,
             background: '#00000077',
