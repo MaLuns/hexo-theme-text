2437e775b97b9eb43b8efb45fb6ca7862b002857
白云苍狗 <384399021@qq.com>
2021-11-07 18:51:44 +0800

    upd: 优化评论组件

--- src/comment/index.js
+++ src/comment/index.js
@@ -1,8 +1,8 @@
 import css from './index.less'
 import html from './index.html'
-/* import Emoji from './emojis/index' */
-import { detect, createList, pathToData, escape, unescape } from './libs/util'
+import { detect, createList, pathToData, create, createEmotesNav, createEmotesList, insertAtCaret, escape, unescape } from './libs/util'
 import marked from 'marked';
+import MD5 from 'md5.js'
 import DOMPurify from 'dompurify';
 import tcp from './libs/tcb'
 const hanabi = require('hanabi');
@@ -45,36 +45,56 @@ class ComComment extends HTMLElement {
         this.nick_input = shadowRoot.getElementById('nick');
         this.email_input = shadowRoot.getElementById('email');
         this.link_input = shadowRoot.getElementById('link');
+        this.emoji_nav = shadowRoot.querySelector('.emojis-container');
+        this.emoji_nav.appendChild(createEmotesNav())
+        this.emojis_list = shadowRoot.querySelector('.emojis-list')
+        this.emotes = {}
 
-        let textarea_hidden = this.textarea_hidden = shadowRoot.getElementById("content_hidden");
         this.textarea = shadowRoot.getElementById("textarea");
         this.sendBtn = shadowRoot.getElementById("send-btn");
 
-        //输入
-        this.textarea.addEventListener('input', function () {
-            textarea_hidden.innerHTML = DOMPurify.sanitize(marked(this.value));
-            this.style.height = textarea_hidden.offsetHeight + 50 + 'px';
+        // 编辑框
+        this.textarea.addEventListener('click', () => {
+            this.lastEditRange = shadowRoot.getSelection().getRangeAt(0)
+        })
+        /* this.textarea.addEventListener('keyup', () => {
+            this.lastEditRange = shadowRoot.getSelection().getRangeAt(0)
+        }) */
+        this.textarea.addEventListener('blur', () => {
+            let editor = shadowRoot.querySelector('.editor-container ')
+            if (this.textarea.innerText !== '') {
+                editor.classList.remove('placeholder-shown')
+            } else {
+                editor.classList.add('placeholder-shown')
+            }
         })
 
         //发送
         this.sendBtn.addEventListener('click', () => {
-            let comment = this.textarea.value;
-            let email = this.email_input.value;
-            let nick = this.nick_input.value || 'Anonymous';
-            let link = this.link_input.value;
-            localStorage.setItem('comment_user_info', JSON.stringify({ nick, email, link }))
-            let { browser, version, os, osVersion } = this.detect;
+            let comment = this.textarea.innerText;
+            let email = this.email_input.value.trim();
+            let nick = this.nick_input.value.trim();
+            let link = this.link_input.value.trim();
 
-            if (comment == '') { this.textarea.focus(); return; }
-            if (nick.length < 2) { this.nick_input.focus(); return; }
-            if ((email.length < 6 || email.indexOf('@') < 1 || email.indexOf('.') < 3) && email.length > 0) { this.email_input.focus(); return; }
+            if (nick.length < 2) {
+                this.nick_input.focus(); return;
+            }
+            if (!/^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(email)) {
+                this.email_input.focus(); return;
+            }
+            if (comment == '') {
+                this.textarea.focus(); return;
+            }
 
             let avatar = '';
             if (/^[1-9][0-9]{6,}@qq.com/.test(email)) {
                 let qq = /^[1-9][0-9]{6,}/.exec(email)[0];
+                avatar = qq
+            } else {
+                avatar = new MD5().update(email).digest('hex')
             }
-
-            let parms = {
+            let { browser, version, os, osVersion } = this.detect;
+            this._send({
                 url: location.pathname,
                 ua: navigator.userAgent,
                 browser: `${browser} ${version}`,
@@ -84,14 +104,24 @@ class ComComment extends HTMLElement {
                 email: DOMPurify.sanitize(email),
                 link: DOMPurify.sanitize(link),
                 content: DOMPurify.sanitize(marked(comment)),
-            }
-            this._send(parms);
+            });
+
+            localStorage.setItem('comment_user_info', JSON.stringify({ nick, email, link }))
         });
 
         //回复
-        shadowRoot.getElementById("list-content").addEventListener('click', (e) => {
-            if (e.target.className == 'c-at') {
-                e.target.parentElement.parentElement.querySelector('.list-edit').appendChild(shadowRoot.getElementById('c-comment'));
+        shadowRoot.getElementById("comment-list").addEventListener('click', (e) => {
+            if (e.target.className == 'comment-reply') {
+                let editContainer = e.target.parentElement.parentElement.querySelector(':scope > .comment-edit-container')
+
+                if (editContainer) {
+                    editContainer.appendChild(shadowRoot.getElementById('comment-editor'))
+                } else {
+                    editContainer = create('div', { class: 'comment-edit-container' })
+                    editContainer.appendChild(shadowRoot.getElementById('comment-editor'))
+                    e.target.parentElement.parentElement.appendChild(editContainer);
+                }
+
                 let { topid, id, idxpath } = e.target.dataset;
                 let { link = '', nick = '', id: atid = '' } = pathToData(this._commentList, idxpath, 'childer');
                 this.atComment = {
@@ -108,9 +138,37 @@ class ComComment extends HTMLElement {
             this._cancel_reply()
         })
 
+        // 加载更多
         shadowRoot.getElementById("loadmore").addEventListener('click', (e) => {
             this._morelist()
         })
+
+        // 切换表情列表
+        this.emoji_nav.addEventListener('click', (e) => {
+            let id = e.target.dataset.package
+            if (id) {
+                if (!this.emotes[id]) {
+                    this.emotes[id] = createEmotesList(e.target.dataset.package).firstElementChild
+                }
+                for (const key in this.emotes) {
+                    this.emotes[key].classList.remove('show')
+                }
+                if (this.package_id !== id) {
+                    this.package_id = id
+                    this.emotes[id].classList.add('show')
+                    this.emojis_list.appendChild(this.emotes[id])
+                } else {
+                    this.package_id = null
+                }
+            }
+        })
+
+        // 插入表情
+        this.emojis_list.addEventListener('click', (e) => {
+            let text = e.target.dataset.text
+            if (text) insertAtCaret(this.textarea, text, shadowRoot, this.lastEditRange)
+        })
+
     }
 
     /**
@@ -151,7 +209,7 @@ class ComComment extends HTMLElement {
         let data = await this._dbs.getComment();
         this._commentList = this._commentList.concat(data)
 
-        this._listContent = this._listContent || this.shadowRoot.querySelector(".list-content");
+        this._listContent = this._listContent || this.shadowRoot.querySelector(".comment-list");
         this._listContent.appendChild(createList(data));
 
         loading.style.display = 'none';
@@ -185,14 +243,14 @@ class ComComment extends HTMLElement {
             if (result.success) {
                 let param = { ...parms, ...result.data }
                 if (!this.atComment) {
-                    let con = this.shadowRoot.querySelector(".list-content");
+                    let con = this.shadowRoot.querySelector(".comment-list");
                     let length = con.querySelectorAll('.item-top').length;
                     con.insertBefore(createList([param]), con.children[length])
 
                     this._commentList.splice(length, 0, param);
 
                 } else {
-                    let con = this.shadowRoot.querySelector("#quote" + this.atComment.topID);
+                    let con = this.shadowRoot.querySelector("#reply" + this.atComment.topID);
                     param._idxpath = this.atComment.topID + ',' + con.children.length
                     con.appendChild(createList([param], this.atComment.topID))
                     let _com = this._commentList.find(item => item.id === this.atComment.topID)
@@ -202,17 +260,14 @@ class ComComment extends HTMLElement {
                     }
                     this._cancel_reply()
                 }
-                this.textarea_hidden.innerHTML = ''
-                this.textarea.value = ''
-                this.textarea.style.height = '40px';
+                this.textarea.innerText = ''
             }
             this.sending = false;
-
         }
     }
 
     _cancel_reply() {
-        this.shadowRoot.querySelector(".edit-container").appendChild(this.shadowRoot.getElementById('c-comment'));
+        this.shadowRoot.querySelector(".comment-edit-container").appendChild(this.shadowRoot.getElementById('comment-editor'));
         this.atComment = null
     }
 
